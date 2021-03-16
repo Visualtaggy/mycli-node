42adb9fd1a4f3001a3263cd288a6a3fa06c48a90
VisualTaggy <visualtaggy@gmail.com>
2021-03-16 17:00:28 +0530

    + create folder function

---
+++ Commands/Organize.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let p = require("path");
+//TEMP CODE
+let input = process.argv.slice(2);
+var dir = input[0];
+
+function createFolder(dirPath) {
+  let folderNames = ["Media", "Archieves", "Documents", "App"];
+  for (let i = 0; i < folderNames.length; i++) {
+    let folderPath = p.join(dirPath, folderNames[i]);
+    if (!fs.existsSync(folderPath)) {
+      fs.mkdirSync(folderPath);
+    }
+  }
+}
+
+createFolder(dir);
